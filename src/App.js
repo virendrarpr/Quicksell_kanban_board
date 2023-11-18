666e8a06197066472c4289c88400ffef3b853048
virendrarpr <virendrapratapsingh2002@gmail.com>
2023-11-18 19:03:33 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import './App.css';
+import TopNav from './components/TopNav/TopNav';
+// import Card from './components/Card/Card';
+import DashView from './components/DashBoard/DashView';
+import { useDispatch, useSelector} from 'react-redux'
+import { fetchAllData } from './Actions/DataAction';
+import Loading from './components/Loading/Loading';
+
+const App = () => {
+  const dispatch = useDispatch();
+  const {allTickets, allUser} = useSelector(state => state.DataReducer);
+  console.log("All Users=", allUser);
+   
+  useEffect(() => {
+    dispatch(fetchAllData());
+  }, [dispatch])
+
+  return allTickets ? (
+    <div style={{paddingTop : "10px"}} >
+      <TopNav/>
+      <hr style={{marginTop : "10px"}} />
+      <DashView/>
+    </div>
+  ) : <Loading/>
+}
+
+export default App
---
+++ src/components/DashBoard/DashView.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { DiCodeigniter } from "react-icons/di";
+import { AiOutlinePlus } from "react-icons/ai";
+import "./DashView.css";
+import Card from "../Card/Card";
+
+const DashView = () => {
+  const { selectedData, user } = useSelector(
+    (state) => state.SelectDataReducer
+  );
+
+  console.log("selectedData = ", selectedData);
+
+  return (
+    selectedData && (
+      <div className="dashContainer">
+        {selectedData.map((elem, index) => {
+          const username = elem[index]?.title;
+          // console.log("userdata=", elem);
+          return (
+            <>
+              <div key={index} className="dashCardContainer">
+                <div className="dashCardHeading flex-sb">
+                  <div className="leftView">
+                    {!user ? (
+                      <DiCodeigniter />
+                    ) : ( 
+                      <>
+                        <div
+                          className="imageContainer relative"
+                          style={{ width: "15px", height: "15px", display : 'inline-block' }}
+                        >
+                          <img
+                            style={{
+                              width: "100%",
+                              height: "100%",
+                              borderRadius: "50%",
+                            }}
+                            src={"https://ui-avatars.com/api/?background=random&color=ff&name="+username}
+                            alt="UserImage"
+                          />
+                        </div>
+                      </>
+                    )}
+                    <span>
+                      {" "}
+                      {elem[index]?.title} {elem[index]?.value?.length}
+                    </span>
+                  </div>
+                  <div className="rightView">
+                    <AiOutlinePlus />{" "}
+                    <span style={{ letterSpacing: "2px" }}>...</span>
+                  </div>
+                </div>
+                <div className="dashList flex-gap-10">
+                  {elem[index]?.value?.map((elem, ind) => {
+                    // console.log("virendar=",elem);
+                    return (
+                      <Card id={elem.id} title={elem.title} tag={elem.tag} username={username} userId={elem.userId}/>
+                    );
+                  })}
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    )
+  );
+};
+
+export default DashView;
